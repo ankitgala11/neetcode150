03ce9bc49946f287ab10a88b07037c99edb02471
Ankit Gala <58542521+ankitgala11@users.noreply.github.com>
2026-05-20 23:03:07 +0530

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.py
@@ -0,0 +1,48 @@
+
+class Solution:
+
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total & 1:
+            return False
+
+        n = len(nums)
+
+        def solve(i, s):
+            if s==0:
+                return True
+            if i>=n or s<0:
+                return False
+
+            if dp[i][s]!=-1:
+                return dp[i][s]
+            
+            take = solve(i+1, s-nums[i])
+            nottake = solve(i+1, s)
+
+            dp[i][s] =  take or nottake
+            return dp[i][s]
+
+        s = total//2
+        dp = [[-1]*(s+1) for _ in range(n)]
+        return solve(0, s)
+
+        def solveTab():
+            t = total//2
+            dp = [[False]*(t+1) for _ in range(n+1)]
+
+            for i in range(n+1):
+                dp[i][0]=True
+
+            for i in range(n-1, -1, -1):
+                for s in range(t+1):
+                    take=False
+                    if s-nums[i]>=0:
+                        take = dp[i+1][s-nums[i]]
+                    nottake = dp[i+1][s]
+
+                    dp[i][s] =  take or nottake
+
+            return dp[0][t]
+
+        # return solveTab()
