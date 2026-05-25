439f2feb638d4a181cc10cdcde0c09ec9c99a34d
Ankit Gala <58542521+ankitgala11@users.noreply.github.com>
2026-05-25 21:58:05 +0530

    Add: jump-game - submission-5

---
+++ Data Structures & Algorithms/jump-game/submission-5.py
@@ -0,0 +1,40 @@
+import sys
+sys.setrecursionlimit(10000)
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        n = len(nums)
+
+        # def solve(i):
+        #     if i>=n-1:
+        #         return True
+            
+        #     if i in dp:
+        #         return dp[i]
+            
+        #     for jump in range(1,1+nums[i]):
+        #         if solve(i+jump):
+        #             return True
+            
+        #     dp[i] =  False
+        #     return False
+
+        # dp = {}
+        # return solve(0)
+        def solveTab():
+
+            dp = [False]*(n)
+            dp[-1] = True
+
+            for i in range(n-2, -1, -1):
+               
+                for jump in range(1,1+nums[i]):
+                    if i+jump<=n-1:
+                        if dp[i+jump]:
+                            dp[i] = True
+                            
+
+
+            return dp[0]
+
+        return solveTab()
