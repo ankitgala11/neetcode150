e98cbb81d12fac0029c3cabd535e07fee6f2d08e
Ankit Gala <58542521+ankitgala11@users.noreply.github.com>
2026-05-25 22:06:08 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        # def solve(i):
+        #     if i>=n-1:
+        #         return 0
+        #     if dp[i]!=-1:
+        #         return dp[i]
+            
+        #     ans = n
+        #     for jump in range(1, nums[i]+1):
+        #         ans = min(ans , 1+ solve(i+jump))
+            
+        #     dp[i] = ans
+        #     return ans
+        
+        # dp = [-1]*n
+        # return solve(0)
+        def solveTab():
+            dp = [0]*n
+
+            for i in range(n-2, -1, -1):
+                ans = n
+                for jump in range(1, nums[i]+1):
+                    if i+jump<n:
+                        ans = min(ans , 1+ dp[i+jump])
+            
+                dp[i] = ans
+
+            return dp[0]
+        
+        
+        return solveTab()
+            
